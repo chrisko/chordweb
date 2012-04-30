91b06ce349a916f2ff27483909d710026084062b
Chris Koenig <chrisjameskoenig@gmail.com>
2012-04-30 11:44:12 -0400

    Fixing message-passing logic, adding more message type handlers. Implementing most of join logic, including server-side node list.

--- server.js
+++ server.js
@@ -10,27 +10,85 @@ var config = {
     port: 5157
 };
 
+var nodes = { };
+
 ////////////////////////////////////////////////////////////////////////////////
 var server = express.createServer();
-var io = socketio.listen(server);
-
-io.sockets.on("connection", function (socket) {
-    console.log("socket.io client connected!");
-    socket.on("JOIN", function (data) {
-        console.log("Received JOIN request!");
-        console.log(data);
-    });
-});
-
 server.configure(function () {
     server.use(express.static("site/static/"));
     server.use(express.errorHandler());
 });
 
-////////////////////////////////////////////////////////////////////////////////
-//server.get("/", function (req, res) {
-//    res.sendfile("src/static/index.html");
-//});
+var io = socketio.listen(server);
+io.configure(function () {
+    io.set("log level", 1);  // Reduce logging.
+
+    // Production settings, from socket.io documentation:
+    io.enable("browser client minification");
+    io.enable("browser client etag");
+    io.enable("browser client gzip");
+});
+
+io.sockets.on("connection", function (socket) {
+    socket.on("message", function (message) {
+        if (message.type == "join request") {
+            // Add this new node by its chosen key:
+            if (!message.requester_key) {
+                console.log("Received join request without a key. Dropping.");
+                return;
+            }
+
+            if (!message.requester_key.match(/^[0-9a-f]{40}$/i)) {
+                console.log("Node tried to join with bad key: \""
+                            + message.requester_key + "\". Ignoring.");
+                return;
+            }
+
+            // If there's no destination, assign one:
+            if (!nodes[message.destination]) {
+                var available_nodes = Object.keys(nodes);
+                if (available_nodes.length) {
+                    // Choose a node at random, if none was specified:
+                    var chosen_index = Math.floor(Math.random() * nodes.length);
+                    message.destination = available_nodes[chosen_index];
+                } else {
+                    // If we don't have any nodes on file, do a self-join:
+                    message.destination = message.requester_key;
+                }
+            }
+
+            // And add the new node in a couple different places:
+            nodes[message.requester_key] = socket;
+            socket.key = message.key;
+            // And tell everyone there's a new node in town:
+            io.sockets.emit("news", { type: "+", node: socket.key });
+        }
+
+        if (!message.destination) {
+            console.log("Received message without destination:");
+            return;
+        }
+
+        if (!nodes[message.destination]) {
+            console.log("Received message for " + message.destination
+                        + ", but there's no such node. Dropping.");
+            return;
+        }
+
+        var destination_socket = nodes[message.destination];
+        destination_socket.emit("message", message);
+    });
+
+    socket.on("disconnect", function () {
+        if (socket.key) {
+            // Remove this node from our list, assuming it's there:
+            delete nodes[socket.key];
+            // And emit the news to all listening clients out there:
+            io.sockets.emit("news", { type: "-", node: socket.key });
+        }
+    });
+});
+
 
 ////////////////////////////////////////////////////////////////////////////////
 server.listen(config.port);
--- src/client/chordweb.js
+++ src/client/chordweb.js
@@ -7,20 +7,47 @@ var TIMEOUT = 3;  // 3s
 
 function ChordWeb(event_bus) {
     this.event_bus = event_bus;
-    this.connection = io.connect("");  // "" == Autodiscover.
+    this.socket = io.connect("");  // "" == Autodiscover.
 
     this.key = Crypto.SHA1(Math.random().toString());
     this.predecessor = null;
     this.successor = null;
 
-    _.bindAll(this, "send_join_request");
-    this.event_bus.subscribe("localhost:wants_to_join", this.send_join_request);
+    var cw = this;
+    this.socket.on("message", function (message) {
+        if (!message.type) {
+            console.log("Received message without type. Dropping.");
+            return;
+        }
+
+        var handler_name = cw.handlers[message.type];
+        if (!handler_name) {
+            console.log("No handler for message type %s", message.type);
+            return;
+        }
+
+        // And finally call the appropriate handler, sending the message in:
+        _.bind(cw[handler_name], cw)(message);
+    });
+
+    this.event_bus.subscribe("localhost:wants_to_join", _.bind(this.send_join_request, this));
 }
+
+ChordWeb.prototype.handlers = {
+    "join request": "process_join_request",
+    "join response": "process_join_response",
+    "leave request": "process_leave_request",
+    "leave response": "process_leave_response",
+    "check request": "process_check_request",
+    "check response": "process_check_response",
+    "stabilize request": "process_stabilize_request",
+    "stabilize response": "process_stabilize_response"
+};
 
 ChordWeb.prototype.is_joined = function () {
     // If our successor is set, we're part of a Chord network.
     return (successor ? true : false);
-}
+};
 
 ChordWeb.prototype.is_key_in_our_range = function (k) {
     // First, if we're the only node in the network, the answer's yes:
@@ -43,15 +70,33 @@ ChordWeb.prototype.is_key_in_our_range = function (k) {
 
 // Joining Logic ///////////////////////////////////////////////////////////////
 ChordWeb.prototype.send_join_request = function () {
-    console.log("Sending join request!");
-    //this.connection.send? publish?
+    this.socket.emit("message", {
+        type: "join request",
+        requester_key: this.key
+    });
 };
 
 ChordWeb.prototype.process_join_request = function (message) {
+    // If we sent a join request and there were no nodes already in the Chord
+    // network, then we may have gotten our own request back.
+    if (message.requester_key == this.key) {
+        // Execute a self-join, making us the only node in the Chord network.
+        this.successor = this.key;
+        this.event_bus.publish("localhost:joined");
+        return;
+    }
+
     if (this.is_key_in_our_range(message.requester_key)) {
         // Return a join response to this node.
+        this.socket.emit("message", {
+            type: "join response",
+            destination: message.requester_key,
+            owner_key: this.key
+        });
     } else {
         // Forward the join response on to our successor.
+        message.destination = this.successor;
+        this.socket.emit("message", message);
     }
 };
 
@@ -64,9 +109,10 @@ ChordWeb.prototype.send_leave_request = function () {
     if (!this.is_joined()) return;
 
     if (this.predecessor) {
+        console.log("");
     }
 
-    this.successor
+    //this.successor
 };
 
 ChordWeb.prototype.process_leave_request = function (message) {
@@ -76,28 +122,86 @@ ChordWeb.prototype.process_leave_request = function (message) {
 // Predecessor Check Logic /////////////////////////////////////////////////////
 ChordWeb.prototype.send_check_request = function () {
     if (this.predecessor) {
+        var transaction_id = parseInt(Math.random() * 1000 * 1000);
+
+        this.socket.emit("message", {
+            type: "check request",
+            destination: this.predecessor,
+            transaction_id: transaction_id,
+            requester_key: this.key
+        });
+
+        this.check = {
+            sent_at: new Date(),
+            timer: setTimeout(_.bind(this.handle_check_timeout, this), TIMEOUT * 1000),
+            transaction_id: transaction_id
+        };
     }
+};
+
+ChordWeb.prototype.handle_check_timeout = function () {
+    console.log("Check request timed out! Clearing predecessor.");
+    this.predecessor = null;
+
+    clearTimeout(this.check.timer);
+    delete this.check;
 };
 
 ChordWeb.prototype.process_check_request = function (message) {
     if (this.is_joined()) {
+        this.socket.emit("message", {
+            type: "check response",
+            destination: message.requester_key,
+            transaction_id: transaction_id
+        });
     }
+};
+
+ChordWeb.prototype.process_check_response = function (message) {
+    if (!this.check) {
+        console.log("Received a check response, but nothing's outstanding!");
+        return;
+    }
+
+    if (message.transaction_id != check.transaction_id) {
+        console.log("Received a check response with the wrong transaction id.");
+        return;
+    }
+
+    // If we're here, it was a successful check response. Don't do anything,
+    // really, except *not* clear our predecessor information.
+
+    // Since we got a response, cancel the timer and remove the stored message:
+    clearTimeout(this.check.timer);
+    delete this.check;
 };
 
 // Stabilization Logic /////////////////////////////////////////////////////////
 ChordWeb.prototype.send_stabilize_request = function () {
     if (this.is_joined()) {
-        //this.connection.send
+        var transaction_id = parseInt(Math.random() * 1000 * 1000);
+        this.socket.emit("message", {
+            type: "stabilize request",
+            destination: this.successor,
+            transaction_id: transaction_id,
+            requester_key: this.key
+        });
+
         this.stabilization = {
             sent_at: new Date(),
-            transaction_id: parseInt(Math.random() * 1000 * 1000),
-            timer: setTimer(TIMEOUT * 1000
+            transaction_id: transaction_id,
+            timer: setTimeout(_.bind(this.handle_stabilize_timeout, this), TIMEOUT * 1000)
         };
     }
 };
 
 ChordWeb.prototype.process_stabilize_request = function (message) {
     assert(message.requester_key);
+};
+
+ChordWeb.prototype.handle_stabilize_timeout = function () {
+    console.log("Stabilize message timed out!");
+    // TODO
 };
 
 ChordWeb.prototype.process_stabilize_response = function (message) {
@@ -128,5 +232,5 @@ ChordWeb.prototype.process_stabilize_response = function (message) {
         return;
     }
 
-    this.stabilization.timer
+    // this.stabilization.timer
 };
